7c46149987539a15c2437c7e68845624a3cf0fa3
KallaiT <119456437+KallaiT@users.noreply.github.com>
2024-04-23 08:40:10 +0200

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,9 @@
+const pets = ["kutya", "macska", "alma", "körte"];
+console.log(pets);
+
+console.log("It just works");
+const container = document.querySelector("#container");
+
+let valami = document.createElement("div");
+valami.innerText = "Hello 🤨";
+container.appendChild(valami);
