d9fada8cfce68997fe381c208fb9eeabd1dc935f
tamas <a@a.com>
2024-04-25 08:45:42 +0200

    New pic in gallery 2

--- gallery.js
+++ gallery.js
@@ -1,15 +1,23 @@
-const pets = ['kutya', 'macska', 'alma', 'körte']
-console.log(pets)
+const pets = ["kutya", "macska", "alma", "körte"];
+console.log(pets);
 
-console.log('It just works')
-const container = document.querySelector('#container')
+console.log("It just works");
+const container = document.querySelector("#container");
 
-let valami = document.createElement('div')
-valami.innerText = 'Hello 🤨'
-container.appendChild(valami)
+let valami = document.createElement("div");
+valami.innerText = "Hello 🤨";
+container.appendChild(valami);
 
 for (const element of pets) {
-  let tdiv = document.createElement('div')
-  container.appendChild(tdiv)
-  tdiv.innerHTML = element
+  let tdiv = document.createElement("div");
+  container.appendChild(tdiv);
+  tdiv.innerHTML = element;
+}
+const istentudja = document.getElementById("de");
+const kepek = ["kutya.jpg", "delfin.jpg", "cica.jpg"];
+for (let i = 0; i < kepek.length; i++) {
+  const img = document.createElement("img");
+  img.src = "img/" + kepek[i];
+  img.width = "500";
+  istentudja.appendChild(img);
 }
